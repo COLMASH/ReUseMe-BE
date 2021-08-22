4c76c4512c2aa9a0ee75a92138deb03f36d94906
Miguel Santana H <mi_santa@hotmail.com>
2021-08-22 06:32:58 -0500

    permite actualizar item posts

--- src/routes/item.js
+++ src/routes/item.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const itemController = require("../controller/item.controller");
-const { auth, itemFilter, formData } = require("../utils/middlewares");
+const { auth, formData } = require("../utils/middlewares");
 
 router.route("/itemCreate").post(auth, formData, itemController.create);
 router.route("/itemList").get(itemController.list);
 router.route("/itemInfo").get(auth, itemController.show);
 router.route("/itemSuscribed").get(auth, itemController.showSuscribed);
-router.route("/itemUpdate").put(itemFilter, itemController.update);
+router.route("/itemUpdate").put(auth, formData, itemController.update);
 router.route("/itemDelete").delete(itemController.destroy);
 
 module.exports = router;
